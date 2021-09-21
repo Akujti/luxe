d476daa41d90553664873f8ce437e83719492116
Edin Vllaço <edin.vllaco@gmail.com>
2021-09-21 17:26:53 +0200

    Create 500.blade.php

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>LUXE - PROPERTIES LLC</title>
+
+</head>
+<body>
+<div class="container h-100">
+    <div class="row justify-content-center h-100">
+        <div class="col-md-5 align-self-center text-center">
+            <h1>Under Maintenance</h1>
+            <img src="https://icon-library.com/images/website-maintenance-icon/website-maintenance-icon-3.jpg" alt="">
+        </div>
+    </div>
+</div>
+</body>
+</html>
