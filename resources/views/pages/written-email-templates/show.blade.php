cd1bb3b05598cf6b0107b862dddd3001a368b3e3
Edin Vllaço <edin.vllaco@gmail.com>
2021-10-04 13:51:47 +0200

    Update show.blade.php

--- resources/views/pages/written-email-templates/show.blade.php
+++ resources/views/pages/written-email-templates/show.blade.php
@@ -88,31 +88,12 @@
 
 @section('js')
     <script>
-        // function copyToClipboard() {
-        //     var copyText = document.getElementById("modal-content");
-        //     navigator.clipboard.writeText(copyText.value);
-        //     toastr.success("Copied");
-        // }
-
         function copyToClipboard() {
             var copyText = document.getElementById("modal-content");
-            if (navigator.clipboard && window.isSecureContext) {
-                return navigator.clipboard.writeText(copyText.value);
-            } else {
-                let textArea = document.createElement("textarea");
-                textArea.value = copyText.value;
-                textArea.style.position = "fixed";
-                textArea.style.left = "-999999px";
-                textArea.style.top = "-999999px";
-                document.body.appendChild(textArea);
-                textArea.focus();
-                textArea.select();
-                return new Promise((res, rej) => {
-                    document.execCommand('copy') ? res() : rej();
-                    textArea.remove();
-                });
-            }
+            navigator.clipboard.writeText(copyText.value);
+            toastr.success("Copied");
         }
+
 
         function populateModal(val) {
             document.getElementById('exampleModalLongTitle').value = val.getAttribute('data-title')
