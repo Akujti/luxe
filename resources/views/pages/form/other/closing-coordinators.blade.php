da92d8059ddebeb7105cc9480f1fac87d7d95f1a
Art <art.kujtimi@hotmail.com>
2021-11-17 18:17:24 +0100

    Update closing-coordinators.blade.php

--- resources/views/pages/form/other/closing-coordinators.blade.php
+++ resources/views/pages/form/other/closing-coordinators.blade.php
@@ -13,6 +13,7 @@
                 <input type="hidden" name="form_title" value="CLOSING COORDINATORS">
                 <input type="hidden" name="to_email[]" value="marketing@luxeknows.com">
                 <input type="hidden" name="to_email[]" value="wesley@luxeknows.com">
+                <input type="hidden" name="to_email[]" value="wesley@luxeknows.com" id="agent_email_form">
                 <div class="card-body">
                     <div class="row">
                         <div class="form-group col-md-6">
@@ -95,9 +96,7 @@
 <script>
     function addAgentEmail(){
         if(getUrlParameter('agent_email'))
-            $('[name="to_email[]"]').val(getUrlParameter('agent_email'))
-        else
-            $('[name="to_email[]"]').val('wesley@luxeknows.com')
+            $('#agent_email_form').val(getUrlParameter('agent_email'))
     }
     var getUrlParameter = function getUrlParameter(sParam) {
         var sPageURL = window.location.search.substring(1),
