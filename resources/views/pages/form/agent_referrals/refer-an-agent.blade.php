38d92634582ee3c6b8b4bb624d83bd49b1f1cc10
Art <art.kujtimi@hotmail.com>
2022-03-01 15:31:14 +0100

    Update refer-an-agent.blade.php

--- resources/views/pages/form/agent_referrals/refer-an-agent.blade.php
+++ resources/views/pages/form/agent_referrals/refer-an-agent.blade.php
@@ -15,6 +15,7 @@
                 <input type="hidden" name="to_email[]" value="support@luxeknows.com">
                 <input type="hidden" name="to_email[]" value="receptionist@luxeknows.com">
                 <input type="hidden" name="to_email[]" value="wesley@luxeknows.com">
+                <input type="hidden" name="to_email[]" value="yesenia@luxeknows.com">
                 <div class="card-body">
                     <div class="row">
                         <div class="form-group col-md-6">
