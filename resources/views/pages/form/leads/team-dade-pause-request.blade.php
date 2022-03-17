29365ee94d3eb17aac46949de65f1684236ebf34
Art <98087293+ajroniedits@users.noreply.github.com>
2022-03-17 10:52:04 +0100

    Update team-dade-pause-request.blade.php

--- resources/views/pages/form/leads/team-dade-pause-request.blade.php
+++ resources/views/pages/form/leads/team-dade-pause-request.blade.php
@@ -12,54 +12,54 @@
                 <input type="hidden" name="form_title" value="Team Dade - Pause Request">
                 <input type="hidden" name="to_email[]" value="email@luxeknows.com">
                 <input type="hidden" name="to_email[]" value="jkirkwood316@gmail.com">
-                <input type="hidden" name="to_email[]" value="gabrielas@luxeknows.com>
+                <input type="hidden" name="to_email[]" value="gabrielas@luxeknows.com">
                 <div class=" card-body">
-                <div class="row">
-                    <div class="form-group col-md-6">
-                        <label for="name">Agent Name</label>
-                        <input type="text" name="agent_full_name" class="form-control" required>
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label for="name">Agent Number</label>
-                        <input type="text" name="agent_number" class="form-control" required>
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label for="name">Agent Email</label>
-                        <input type="text" name="agent_email" class="form-control" required>
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label for="name">Zillow Team</label>
-                        <select name="zillow_team" class="form-control" required id="">
-                            <option value="-">-</option>
-                            <option value="Team FLEX">Team FLEX</option>
-                            <option value="Team DADE">Team DADE</option>
-                        </select>
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label for="name">Start Pause Date</label>
-                        <input type="date" name="start_pause_date" class="form-control" required>
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label for="name">End Pause Date</label>
-                        <input type="date" name="end_pause_date" class="form-control" required>
-                    </div>
-                    <div class="form-group col-md-12">
-                        <label for="name">Reason for Zillow Pause</label>
-                        <select name="zillow_team" class="form-control" required id="">
-                            <option value="-">-</option>
-                            <option value="Family">Family</option>
-                            <option value="Sick">Sick</option>
-                            <option value="Vacation">Vacation</option>
-                            <option value="Other">Other</option>
-                        </select>
-                    </div>
-                    <div class="form-group col-12">
-                        <input type="submit" class="btn btn-luxe w-100" value="SUBMIT">
+                    <div class="row">
+                        <div class="form-group col-md-6">
+                            <label for="name">Agent Name</label>
+                            <input type="text" name="agent_full_name" class="form-control" required>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="name">Agent Number</label>
+                            <input type="text" name="agent_number" class="form-control" required>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="name">Agent Email</label>
+                            <input type="text" name="agent_email" class="form-control" required>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="name">Zillow Team</label>
+                            <select name="zillow_team" class="form-control" required id="">
+                                <option value="-">-</option>
+                                <option value="Team FLEX">Team FLEX</option>
+                                <option value="Team DADE">Team DADE</option>
+                            </select>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="name">Start Pause Date</label>
+                            <input type="date" name="start_pause_date" class="form-control" required>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="name">End Pause Date</label>
+                            <input type="date" name="end_pause_date" class="form-control" required>
+                        </div>
+                        <div class="form-group col-md-12">
+                            <label for="name">Reason for Zillow Pause</label>
+                            <select name="zillow_team" class="form-control" required id="">
+                                <option value="-">-</option>
+                                <option value="Family">Family</option>
+                                <option value="Sick">Sick</option>
+                                <option value="Vacation">Vacation</option>
+                                <option value="Other">Other</option>
+                            </select>
+                        </div>
+                        <div class="form-group col-12">
+                            <input type="submit" class="btn btn-luxe w-100" value="SUBMIT">
+                        </div>
                     </div>
                 </div>
+            </form>
         </div>
-        </form>
     </div>
-</div>
 </div>
 @endsection
