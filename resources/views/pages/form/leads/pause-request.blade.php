2b7e6a9b4c7ea20519874c548b08327d0cb98c83
Art <98087293+ajroniedits@users.noreply.github.com>
2022-03-17 16:27:51 +0100

    add email

--- resources/views/pages/form/leads/pause-request.blade.php
+++ resources/views/pages/form/leads/pause-request.blade.php
@@ -13,6 +13,7 @@
                 <input type="hidden" name="to_email[]" value="email@luxeknows.com">
                 <input type="hidden" name="to_email[]" value="zillowluxe@gmail.com">
                 <input type="hidden" name="to_email[]" value="irais@luxeknows.com">
+                <input type="hidden" name="to_email[]" value="operations@luxeknows.com">
                 <div class="card-body">
                     <div class="row">
                         <div class="form-group col-md-6">
