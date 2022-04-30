f854593dc3afe54756b19edb7eef257d01c0882d
Art <98087293+ajroniedits@users.noreply.github.com>
2022-04-30 14:04:06 +0200

    auto fill

--- resources/views/pages/form/agent_referrals/flex-agent-form.blade.php
+++ resources/views/pages/form/agent_referrals/flex-agent-form.blade.php
@@ -21,7 +21,8 @@
                     <div class="row">
                         <div class="form-group col-md-6">
                             <label for="name">Legal Name</label>
-                            <input type="text" name="agent_full_name" class="form-control" required>
+                            <input type="text" name="agent_full_name" class="form-control"
+                                value="{{auth()->user()->profile->fullname}}" required>
                         </div>
                         <div class="form-group col-md-6">
                             <label for="name">If applicable: Preferred Name to be used by Marketing:</label>
@@ -41,7 +42,8 @@
                         </div>
                         <div class="form-group col-md-6">
                             <label for="name">Personal Email:</label>
-                            <input type="text" name="agent_email" class="form-control" required>
+                            <input type="text" name="agent_email" class="form-control" value="{{auth()->user()->email}}"
+                                required>
                         </div>
                         <div class="form-group col-md-6">
                             <label for="name">Date of Birth - Month/Day:</label>
