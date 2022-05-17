fbbdd92e8a78c65d18485c4a2f9bf942dab76045
Art <98087293+ajroniedits@users.noreply.github.com>
2022-05-17 19:56:19 +0200

    Update index.blade.php

--- resources/views/pages/form/leads/index.blade.php
+++ resources/views/pages/form/leads/index.blade.php
@@ -23,26 +23,25 @@
             font-size: 13px;
         }
     }
+
 </style>
 @section('content')
-
-
-<div class="container-fluid d-flex">
-    <div class="row justify-content-center w-100 p-0 m-0 align-items-center" style="height: 70vh">
-        <div class="col-md-12">
-            <div class="">
-                <div class="card-title text-center">
-                    <h1>Leads - Select Form</h1>
-                </div>
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-lg-4 mb-4">
-                            <div class="box">
-                                <a href="{{route('general.email.index',['folder'=>'leads','form'=>'join-zillow'])}}"
-                                    class="btn w-100 d-flex align-items-center justify-content-center">Join Zillow</a>
+    <div class="container-fluid d-flex">
+        <div class="row justify-content-center w-100 p-0 m-0 align-items-center" style="height: 70vh">
+            <div class="col-md-12">
+                <div class="">
+                    <div class="card-title text-center">
+                        <h1>Leads - Select Form</h1>
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-lg-4 mb-4">
+                                <div class="box">
+                                    <a href="{{ route('general.email.index', ['folder' => 'leads', 'form' => 'join-zillow']) }}"
+                                        class="btn w-100 d-flex align-items-center justify-content-center">Join Zillow</a>
+                                </div>
                             </div>
-                        </div>
-                        <div class="col-lg-4 mb-4">
+                            {{-- <div class="col-lg-4 mb-4">
                             <div class="box">
                                 <a href="{{route('general.email.index',['folder'=>'leads','form'=>'team-dade-pause-request'])}}"
                                     class="btn w-100 d-flex align-items-center justify-content-center">Team Dade - Pause
@@ -55,26 +54,27 @@
                                     class="btn w-100 d-flex align-items-center justify-content-center">Team Flex - Pause
                                     Request</a>
                             </div>
-                        </div>
-                        <div class="col-lg-4 mb-4">
-                            <div class="box">
-                                <a href="{{route('general.email.index',['folder'=>'leads','form'=>'request-zillow-nurtures'])}}"
-                                    class="btn w-100 d-flex align-items-center justify-content-center">Request Zillow
-                                    Nurtures</a>
+                        </div> --}}
+                            <div class="col-lg-4 mb-4">
+                                <div class="box">
+                                    <a href="{{ route('general.email.index', ['folder' => 'leads', 'form' => 'request-zillow-nurtures']) }}"
+                                        class="btn w-100 d-flex align-items-center justify-content-center">Request Zillow
+                                        Nurtures</a>
+                                </div>
                             </div>
-                        </div>
-                        <div class="col-lg-4 mb-4">
-                            <div class="box">
-                                <a href="{{route('general.email.index',['folder'=>'leads','form'=>'zillow-leads-weekly-update'])}}"
-                                    class="btn w-100 d-flex align-items-center justify-content-center">Team Flex - <br>
-                                    Zillow Leads Weeky Update</a>
+                            <div class="col-lg-4 mb-4">
+                                <div class="box">
+                                    <a href="{{ route('general.email.index', ['folder' => 'leads', 'form' => 'zillow-leads-weekly-update']) }}"
+                                        class="btn w-100 d-flex align-items-center justify-content-center">Team Flex - <br>
+                                        Zillow Leads Weeky Update</a>
+                                </div>
                             </div>
-                        </div>
-                        <div class="col-lg-4 mb-4">
-                            <div class="box">
-                                <a href="{{route('general.email.index',['folder'=>'leads','form'=>'team-dade-zillow-leads-weekly-update'])}}"
-                                    class="btn w-100 d-flex align-items-center justify-content-center">Team Dade -<br>
-                                    Zillow Leads Weeky Update</a>
+                            <div class="col-lg-4 mb-4">
+                                <div class="box">
+                                    <a href="{{ route('general.email.index', ['folder' => 'leads', 'form' => 'team-dade-zillow-leads-weekly-update']) }}"
+                                        class="btn w-100 d-flex align-items-center justify-content-center">Team Dade -<br>
+                                        Zillow Leads Weeky Update</a>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -82,5 +82,4 @@
             </div>
         </div>
     </div>
-</div>
 @endsection
