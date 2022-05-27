228cd0c6a61ded20cc446cd687d735ed832b402e
fisnikawa <fisnik@ajroni.com>
2022-05-27 15:59:25 +0200

    multiple markers

--- resources/views/optin/index.blade.php
+++ resources/views/optin/index.blade.php
@@ -107,7 +107,7 @@
     </div>
     {{$agents->links()}}
 </div>
-<script src="https://maps.googleapis.com/maps/api/js?key={{ env('GOOGLE_MAPS_API_KEY') }}&callback=initMap&v=weekly"
+<script src="https://maps.googleapis.com/maps/api/js?v=3&key={{ env('GOOGLE_MAPS_API_KEY') }}&callback=initMap&v=weekly"
         defer></script>
 <script src="https://unpkg.com/@googlemaps/markerclusterer/dist/index.min.js"></script>
 <script>
@@ -120,29 +120,38 @@
             center: new google.maps.LatLng(25.751360, -80.255580),
             mapTypeId: google.maps.MapTypeId.ROADMAP
         });
-
         var infowindow = new google.maps.InfoWindow();
 
         var marker, i;
-        const svgMarker = {
-            path: "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
-            fillColor: "black",
-            fillOpacity: 0.72,
-            strokeWeight: 0,
-            rotation: 0,
-            scale: 2,
-            anchor: new google.maps.Point(15, 30),
-        };
+        var AllLatLng = [];
+        
         agents.data.forEach((el) => {
             codeAddress(el.profile.address, function(coords) {
+                AllLatLng.push({
+                    lat: coords[0], lng: coords[1]
+                })
+                const svgMarker = {
+                    path: "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
+                    fillColor: "black",
+                    fillOpacity: 0.72,
+                    strokeWeight: 0,
+                    rotation: 0,
+                    scale: 2,
+                    anchor: new google.maps.Point(15, 30),
+                };
+                if(AllLatLng.filter(x => x.lat == coords[0]).length == 2) {
+                    svgMarker.fillColor = "blue"
+                    svgMarker.anchor = new google.maps.Point(25, 30)
+                }
                 marker = new google.maps.Marker({
-                    position: new google.maps.LatLng(coords[0], coords[1]),
+                    position: new google.maps.LatLng(coords[0], coords[1], true),
                     icon: svgMarker,
                     map: map
                 });
-
+                  
                 google.maps.event.addListener(marker, 'click', (function(marker, i) {
                     return function() {
+                        
                         infowindow.setContent(
                             "<div class='d-flex align-items-center' style='gap:10px;margin-bottom:6px'>" + 
                             "<img style='width:58px;height:58px;border-radius:50%;' src='" + el.avatar + "'>" +
@@ -157,8 +166,7 @@
                 })(marker, i));
             });
         });
-
-        return;
+        var mc = new MarkerClusterer(map, markers)
     }
     function codeAddress(address, callback) {
         let geocoder = new google.maps.Geocoder();
