1e8ea8ba86c9baf4d25a2fad908bf3cc8988e20c
Art <98087293+ajroniedits@users.noreply.github.com>
2022-09-27 22:46:35 +0200

    Update closing-coordinators-agents.blade.php

--- resources/views/pages/form/other/closing-coordinators-agents.blade.php
+++ resources/views/pages/form/other/closing-coordinators-agents.blade.php
@@ -1,101 +1,100 @@
 @extends('layouts.app', ['active' => 'leads_services_support', 'subactive' => 'request_listing_closing_coordinators'])
 @section('css')
-<style>
-    body {
-        /* background: url('{{asset('images/bg-image1.jpg')}}'); */
-        height: 100vh;
-        /* background-size: cover;
-        background-position: center;
-        background-color: rgba(0, 0, 0, 0.7);
-        background-blend-mode: color; */
+    <style>
+        body {
+            /* background: url('{{ asset('images/bg-image1.jpg') }}'); */
+            height: 100vh;
+            /* background-size: cover;
+            background-position: center;
+            background-color: rgba(0, 0, 0, 0.7);
+            background-blend-mode: color; */
 
-    }
+        }
 
-    main,
-    #app {
-        height: 100%;
-    }
+        main,
+        #app {
+            height: 100%;
+        }
 
-    main {
-        display: flex;
-        align-items: center;
-    }
+        main {
+            display: flex;
+            align-items: center;
+        }
 
-    .card-header {
-        font-size: 18px;
-        color: #ffffff;
-        line-height: 22px;
-        text-align: center;
-        padding: 0;
-        background: none;
-        border: none;
-        padding-top: 10px;
-    }
+        .card-header {
+            font-size: 18px;
+            color: #ffffff;
+            line-height: 22px;
+            text-align: center;
+            padding: 0;
+            background: none;
+            border: none;
+            padding-top: 10px;
+        }
 
 
-    a:hover {
-        text-decoration: none;
-    }
+        a:hover {
+            text-decoration: none;
+        }
 
-    .agent-name {
-        margin-top: 20px;
-        /* background: linear-gradient(180deg, rgba(0, 0, 0, .1) 0%, rgba(255, 255, 255, 0.26) 100%); */
-        background-color: #262626;
-        border-radius: 15px;
-        border: 1px solid #fff;
-        padding: 18px 25px;
-        text-transform: uppercase;
-    }
+        .agent-name {
+            margin-top: 20px;
+            /* background: linear-gradient(180deg, rgba(0, 0, 0, .1) 0%, rgba(255, 255, 255, 0.26) 100%); */
+            background-color: #262626;
+            border-radius: 15px;
+            border: 1px solid #fff;
+            padding: 18px 25px;
+            text-transform: uppercase;
+        }
 
-    .agents img {
-        border-radius: 10px;
-        max-height: 350px;
-        object-fit: cover;
-        object-position: top;
-    }
+        .agents img {
+            border-radius: 10px;
+            max-height: 350px;
+            object-fit: cover;
+            object-position: top;
+        }
 
-    .h1-luxe {
-        font-family: 'gothicbold';
-    }
-</style>
+        .h1-luxe {
+            font-family: 'gothicbold';
+        }
+    </style>
 @endsection
 @section('content')
+    <div class="container-fluid agents">
+        <h1 class="text-center h1-luxe">CLOSING COORDINATORS</h1>
+        <div class="row">
+            @foreach ($coordinators as $coordinator)
+                <div class="col-md-3 my-2">
+                    @if (auth()->user() && auth()->user()->isAdmin)
+                        <form action="{{ route('change_status') }}" method="POST">
+                            @csrf
+                            <input type="hidden" name="id" value="{{ $coordinator->id }}">
+                            <button type="submit" class="btn btn-luxe text-center w-100"
+                                style="border-radius: 30px;">{{ $coordinator->status ? 'Hide' : 'Show' }}</button>
+                        </form>
+                    @endif
+                    @if ($coordinator->status)
+                        <div class="bg-transparent">
+                            <a href="{{ route('closing-coordinator', $coordinator->id) }}">
+                                <div class="card-header">
+                                    <img src="{{ asset($coordinator->image) }}" alt="" class="w-100">
+                                    <p class="agent-name">{{ $coordinator->name }}</p>
+                                </div>
+                            </a>
+                        </div>
+                    @else
+                        <div class="bg-transparent">
+                            <div class="card-header">
+                                <img src="{{ asset($coordinator->image) }}" alt="" class="w-100">
+                                <p class="agent-name">Sorry this closing coordinator is not taking any files at the moment
+                                </p>
+                            </div>
+                        </div>
+                    @endif
 
-<div class="container-fluid agents">
-    <h1 class="text-center h1-luxe">CLOSING COORDINATORS</h1>
-    <div class="row">
-        @foreach ($coordinators as $coordinator)
-        <div class="col my-2">
-            @if (auth()->user() && auth()->user()->isAdmin)
-            <form action="{{route('change_status')}}" method="POST">
-                @csrf
-                <input type="hidden" name="id" value="{{$coordinator->id}}">
-                <button type="submit" class="btn btn-luxe text-center w-100" style="border-radius: 30px;">{{$coordinator->status ?
-                    'Hide':'Show'}}</button>
-            </form>
-            @endif
-            @if ($coordinator->status)
-            <div class="bg-transparent">
-                <a href="{{route('closing-coordinator',$coordinator->id)}}">
-                    <div class="card-header">
-                        <img src="{{asset($coordinator->image)}}" alt="" class="w-100">
-                        <p class="agent-name">{{$coordinator->name}}</p>
-                    </div>
-                </a>
-            </div>
-            @else
-            <div class="bg-transparent">
-                <div class="card-header">
-                    <img src="{{asset($coordinator->image)}}" alt="" class="w-100">
-                    <p class="agent-name">Sorry this closing coordinator is not taking any files at the moment
-                    </p>
                 </div>
-            </div>
-            @endif
-
-        </div>
-        @endforeach
-        {{-- <div class="col my-2">
+            @endforeach
+            {{-- <div class="col my-2">
             <div class="bg-transparent">
                 <a
                     href="/general/form/other/closing-coordinators?agent_email=victoria@luxeknows.com&agent_text=Closing Coordinator - Basic Package $200 - VIP Package $325">
@@ -117,8 +116,6 @@
                 </a>
             </div>
         </div> --}}
+        </div>
     </div>
-</div>
-
-
 @endsection
