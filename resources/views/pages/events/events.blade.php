2f8d8d870eedc161156dd9671a00268631fd4a05
Art <98087293+ajroniedits@users.noreply.github.com>
2022-11-24 09:50:59 +0100

    Update events.blade.php

--- resources/views/pages/events/events.blade.php
+++ resources/views/pages/events/events.blade.php
@@ -375,13 +375,13 @@
                 } else {
                     $('#event_attend_form_wrapper').css('display', 'block');
                 }
-                if (event.extendedProps.rsvp != null || {{ $isAdmin ? true : false }}) {
+                if (event.extendedProps.rsvp != null || {{ $isAdmin }}) {
                     $('.single-event').find('#rsvp').attr('href', event.extendedProps.rsvp);
                     $('#rsvp_group').css('display', 'block');
                 } else {
                     $('#rsvp_group').css('display', 'none');
                 }
-                if (event.extendedProps.zoom != null || {{ $isAdmin ? true : false }}) {
+                if (event.extendedProps.zoom != null || {{ $isAdmin }}) {
                     $('.single-event').find('#zoom').attr('href', event.extendedProps.zoom);
                     $('#zoom_group').css('display', 'block');
                 } else {
