8a76822ba57e292de4c1972728af9cbd612c4c71
Art <98087293+ajroniedits@users.noreply.github.com>
2023-01-26 10:39:38 +0100

    Update join-zillow.blade.php

--- resources/views/pages/form/leads/join-zillow.blade.php
+++ resources/views/pages/form/leads/join-zillow.blade.php
@@ -7,7 +7,7 @@
                     <div class="modal-dialog modal-dialog-centered" role="document">
                         <div class="modal-content bg-white">
                             <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLongTitle">Disclaimer
+                                <h5 class="modal-title" id="exampleModalLongTitle">Update From Team
                                 </h5>
                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                     <span aria-hidden="true">&times;</span>
