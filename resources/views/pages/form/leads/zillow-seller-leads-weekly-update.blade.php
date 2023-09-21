66c3904fdb67607d3ce7b9173036f44e73722d47
Art <98087293+ajroniedits@users.noreply.github.com>
2023-09-21 13:03:32 +0200

    NEW FORM

---
+++ resources/views/pages/form/leads/zillow-seller-leads-weekly-update.blade.php
@@ -0,0 +1,104 @@
+@extends('layouts.app', ['active' => 'leads_services_support', 'subactive' => 'get_leads'])
+@section('content')
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <form action="{{ route('general.email.post') }}" class="card form mb-5 p-3" method="POST">
+                    @csrf
+                    <div class="card-header">
+                        <h1 class="text-center my-4">Zillow Seller Leads Weekly Update</h1>
+
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="form-group col-md-6">
+                                <label for="name">Agent Name</label>
+                                <input type="text" name="agent_full_name" class="form-control"
+                                    value="{{ auth()->user()->profile->fullname }}" required>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="name">Email</label>
+                                <input type="text" name="agent_email" class="form-control"
+                                    value="{{ auth()->user()->email }}" required>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="name">Number of <b>Total</b> FULLY executed <b>Zillow</b> Listings. Sorry to
+                                    capitalize fully but we have had some confusion on agents as to what a fully executed
+                                    listing agreement is?</label>
+                                <select name="number_of_fully_executed_listings" class="form-control" required
+                                    id="">
+                                    <option value>-</option>
+                                    <option value="0">0</option>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    <option value="6">6</option>
+                                    <option value="7">7</option>
+                                    <option value="8">8</option>
+                                    <option value="9">9</option>
+                                    <option value="10">10</option>
+                                    <option value="11">11</option>
+                                    <option value="12">12</option>
+                                    <option value="13">13</option>
+                                    <option value="14">14</option>
+                                    <option value="15">15</option>
+                                    <option value="16">16</option>
+                                    <option value="17">17</option>
+                                    <option value="18">18</option>
+                                    <option value="19">19</option>
+                                    <option value="20">20</option>
+                                    <option value="21">21</option>
+                                    <option value="22">22</option>
+                                    <option value="23">23</option>
+                                    <option value="24">24</option>
+                                    <option value="25">25</option>
+                                    <option value="26">26</option>
+                                    <option value="27">27</option>
+                                    <option value="28">28</option>
+                                    <option value="29">29</option>
+                                    <option value="30">30</option>
+                                </select>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="name">Please list the addresses of the fully executed Zillow Listings along
+                                    with the Seller Name as it appears on Zillow</label>
+                                <textarea name="agent_email" class="form-control" required></textarea>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="name">Were any of the contracts above executed in the last 7 days?</label>
+                                <select name="were_any_of_the_contracts_above_executed_in_the_last_7_days"
+                                    class="form-control" required>
+                                    <option value>-</option>
+                                    <option value="Yes">Yes</option>
+                                    <option value="No">No</option>
+                                </select>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="name">Have you cancelled any Zillow listings in the past 7 days?</label>
+                                <select name="have_you_cancelled_any_zillow_listings_in_the_past_7_days"
+                                    class="form-control" required>
+                                    <option value>-</option>
+                                    <option value="Yes">Yes</option>
+                                    <option value="No">No</option>
+                                </select>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="name">Are you currently paused?</label>
+                                <select name="are_you_currently_paused" class="form-control" required>
+                                    <option value>-</option>
+                                    <option value="Yes">Yes</option>
+                                    <option value="No">No</option>
+                                </select>
+                            </div>
+                            <div class="form-group form-footer col-12">
+                                <input type="submit" class="btn btn-luxe w-100" value="SUBMIT">
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
