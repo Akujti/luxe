adfabae21612a095ab6f3b6a88e45d7d1273f9be
Art Kujtimi <edits@ajroni.com>
2023-11-21 16:49:39 +0100

    Update index.blade.php

--- resources/views/luxe-staff/index.blade.php
+++ resources/views/luxe-staff/index.blade.php
@@ -93,7 +93,6 @@
                 <p class="staff-role">Agent Experience Specialist</p>
                 <p class="staff-email">onboarding@luxeknows.com</p>
             </div>
-            ØØ
             <div class="col-md-3 staff-box">
                 <img src="{{ asset('images/staff/yesenia.jpg') }}" class="staff-image">
                 <p class="staff-name">Yesenia Ferro</p>
@@ -113,17 +112,17 @@
                 <p class="staff-email">onboarding@luxeknows.com</p>
             </div> --}}
             <!-- <div class="col-md-3 staff-box">
-                                                                    <img src="{{ asset('images/staff/user.jpg') }}" class="staff-image">
-                                                                    <p class="staff-name">Roxie Cordoba</p>
-                                                                    <p class="staff-role">Lead Admin</p>
-                                                                    <p class="staff-email">leadadmin@luxeknows.com</p>
-                                                                </div>
-                                                                <div class="col-md-3 staff-box">
-                                                                    <img src="{{ asset('images/staff/albert.jpg') }}" class="staff-image">
-                                                                    <p class="staff-name">Albert Babani</p>
-                                                                    <p class="staff-role">Sales Coach</p>
-                                                                    <p class="staff-email">albert@babaniproperties.com</p>
-                                                                </div> -->
+                                                                        <img src="{{ asset('images/staff/user.jpg') }}" class="staff-image">
+                                                                        <p class="staff-name">Roxie Cordoba</p>
+                                                                        <p class="staff-role">Lead Admin</p>
+                                                                        <p class="staff-email">leadadmin@luxeknows.com</p>
+                                                                    </div>
+                                                                    <div class="col-md-3 staff-box">
+                                                                        <img src="{{ asset('images/staff/albert.jpg') }}" class="staff-image">
+                                                                        <p class="staff-name">Albert Babani</p>
+                                                                        <p class="staff-role">Sales Coach</p>
+                                                                        <p class="staff-email">albert@babaniproperties.com</p>
+                                                                    </div> -->
         </div>
     </div>
 @endsection
