32f4769b88f2390dba41ef89a6474e46f617912f
Art Kujtimi <edits@ajroni.com>
2024-02-26 15:47:43 +0100

    incentives

--- resources/views/pages/luxe_incentives.blade.php
+++ resources/views/pages/luxe_incentives.blade.php
@@ -9,9 +9,26 @@
             font-family: 'Montserrat'
         }
 
+        ul.list-unstyled {
+            list-style: none;
+            padding: 0;
+        }
+
         ul.list-unstyled li {
             margin-bottom: 15px;
             font-size: 20px;
+
+        }
+
+        ul.inside-text li {
+            display: flex;
+            /* align-items: center; */
+        }
+
+        ul.inside-text li::before {
+            content: "•";
+            margin-right: 5px;
+            /* Adjust as needed */
         }
 
         h2.under-contract {
@@ -46,21 +63,21 @@
         }
     </style>
     <div class="container-fluid mb-4">
-        <div class="row justify-content-center">
-            <div class="col-md-7">
+        <div class="row">
+            <div class="col-md-6">
                 <div class="text-center">
                     <h2 class="under-contract my-5">LUXE INCENTIVES</h2>
                 </div>
                 <h4 class="text-center">2023 TOP PRODUCERS</h4>
-                <p class="text-center mb-5">Yearly Top Producers are reviewed in January for the previous calendar year and
+                <p class="text-center">Yearly Top Producers are reviewed in January for the previous calendar year and
                     marketing budgets are released in February.</p>
-                <ul class="list-unstyled my-3">
+                <ul class="list-unstyled my-5">
                     <li>★ $6 Million in Yearly Sales - $250 Monthly Marketing Budget</li>
                     <li>★ $11 Million in Yearly Sales - $500 Monthly Marketing Budget</li>
                     <li class="mb-0">★ $16 Million in Yearly Sales - $1,000 Monthly Marketing Budget </li>
                 </ul>
                 <p>Please take a moment to review the below:</p>
-                <ul class="pl-0" style="list-style-position: inside;">
+                <ul class="pl-0 inside-text" style="list-style-position: inside;">
                     <li class="ms-0">Yearly top producers are the top 5% of agents who have been with the brokerage for at
                         least 6 months
                         or more.</li>
@@ -69,8 +86,9 @@
                     <li>Marketing budgets will have set duration periods.</li>
                     <li>Company deals such as Zillow and CINC are not included.</li>
                 </ul>
-                <hr class="my-5">
-                <h4 class="text-center">LEGACY INCENTIVES</h4>
+            </div>
+            <div class="col-md-6">
+                <h2 class="under-contract my-5 text-center">LEGACY INCENTIVES</h2>
                 <div class="row text-center align-items-end">
                     <h4 class="col-12">$10 Million Dollar Club</h4>
                     <div class="col-md-6">
@@ -84,34 +102,24 @@
                 </div>
                 <hr>
                 <div class="row text-center justify-content-center">
-                    <h4 class="col-12">$25 Million Dollar Club</h4>
                     <div class="col-md-6">
-                        <img src="/images/incentives/macbook.png" alt="" class="incentive-img">
+                        <h4 class="col-12">$25 Million Dollar Club</h4>
+                        <img src="/images/incentives/macbook.jpg" alt="" class="incentive-img shadow-img">
                         <p>Macbook Pro</p>
                     </div>
-                </div>
-                <hr>
-                <div class="row text-center justify-content-center">
-                    <h4 class="col-12">$50 Million Dollar Club</h4>
                     <div class="col-md-6">
+                        <h4 class="col-12">$50 Million Dollar Club</h4>
                         <img src="/images/incentives/hotel.jpg" alt="" class="incentive-img shadow-img">
-                    </div>
-                    <div class="col-12">
                         <p>3-Night Trip to All-Inclusive Resort - Tropical Destination</p>
                     </div>
-                </div>
-                <hr>
-                <div class="row text-center justify-content-center">
-                    <h4 class="col-12">$75 Million Dollar Club</h4>
+                    <hr class="col-12">
                     <div class="col-md-6">
+                        <h4 class="">$75 Million Dollar Club</h4>
                         <img src="/images/incentives/watch.png" alt="" class="incentive-img" height="200px">
                         <p>Rolex Oyster Perpetual</p>
                     </div>
-                </div>
-                <hr>
-                <div class="row text-center justify-content-center">
-                    <h4 class="col-12">$100 Million Dollar Club</h4>
                     <div class="col-md-6">
+                        <h4>$100 Million Dollar Club</h4>
                         <img src="/images/incentives/paris.jpg" alt="" class="incentive-img shadow-img">
                         <p>10-Day Trip to Europe</p>
                     </div>
