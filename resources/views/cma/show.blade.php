7f913b5ee0d8b26f3b58994a41c0fd1c6bca3852
Art Kujtimi <edits@ajroni.com>
2024-05-13 10:39:34 +0200

    Update show.blade.php

--- resources/views/cma/show.blade.php
+++ resources/views/cma/show.blade.php
@@ -286,9 +286,6 @@
                                                                                 <option value="Commercial Sale">
                                                                                     Commercial Sale
                                                                                 </option>
-                                                                                <option value>
-                                                                                    Other
-                                                                                </option>
                                                                             </select>
                                                                         </div>
                                                                     </div>
