3445a3e29ecd045b7049ea71f42d36eb4a6a3e35
Art Kujtimi <edits@ajroni.com>
2024-05-21 16:15:10 +0200

    Update newconstructionsearch.blade.php

--- resources/views/pages/newconstructionsearch.blade.php
+++ resources/views/pages/newconstructionsearch.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app', ['active' => 'leads_services_support', 'subactive' => 'newconstructionsearch'])
+@extends('layouts.app', [ 'subactive' => 'newconstructionsearch'])
 @section('content')
     <script src="https://spread.name/js/widget.js"></script>
     <div data-ss-widget data-id="ni24kmm" data-routing="none"></div>
