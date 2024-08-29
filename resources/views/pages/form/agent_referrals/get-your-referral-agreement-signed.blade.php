0388661545d396093841f991da9ccfaa724c3c42
edin-vllaco <edin@ajroni.com>
2024-08-29 20:22:35 +0200

    Update get-your-referral-agreement-signed.blade.php

--- resources/views/pages/form/agent_referrals/get-your-referral-agreement-signed.blade.php
+++ resources/views/pages/form/agent_referrals/get-your-referral-agreement-signed.blade.php
@@ -4,11 +4,13 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
 
-                <form action="{{ route('general.email.post') }}" class="card form p-3" method="POST">
+                <form action="{{ route('general.email.post') }}" enctype="multipart/form-data" class="card form p-3"
+                      method="POST">
                     @csrf
                     <div class="card-header">
                         <h1 class="text-center my-4">Get Your Referral Agreement Signed</h1>
-                        <h5 class="text-center">Please upload a referral agreement that is completely filled out and ready
+                        <h5 class="text-center">Please upload a referral agreement that is completely filled out and
+                            ready
                             for review.</h5>
                     </div>
                     <input type="hidden" name="form_title" value="Get Your Referral Agreement Signed">
@@ -17,17 +19,17 @@
                             <div class="form-group col-md-6">
                                 <label for="name">Agent Name</label>
                                 <input type="text" name="agent_full_name" class="form-control"
-                                    value="{{ auth()->user()->profile->fullname }}" required>
+                                       value="{{ auth()->user()->profile->fullname }}" required>
                             </div>
                             <div class="form-group col-md-6">
                                 <label for="name">Agent Phone</label>
                                 <input type="text" name="agent_phone" class="form-control"
-                                    value="{{ auth()->user()->profile->phone }}" required>
+                                       value="{{ auth()->user()->profile->phone }}" required>
                             </div>
                             <div class="form-group col-md-6">
                                 <label for="name">Agent Email</label>
                                 <input type="text" name="agent_email" class="form-control"
-                                    value="{{ auth()->user()->email }}" required>
+                                       value="{{ auth()->user()->email }}" required>
                             </div>
                             <div class="form-group col-md-6">
                                 <label for="name">Upload Referral Agreement</label>
