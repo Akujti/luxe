930bf4c7b9f0cc5facc6635bc3fa69e1e4a139d4
edin-vllaco <edin@ajroni.com>
2024-08-30 16:18:02 +0200

    Update PayPalController.php

--- app/Http/Controllers/PayPalController.php
+++ app/Http/Controllers/PayPalController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class PayPalController extends Controller
@@ -12,43 +11,11 @@ class PayPalController extends Controller
     {
         $data = $request->all();
 
-        Log::info('paypal webhook received');
-
-        Log::info(json_encode($data));
-
-        return;
-
-        // Verify PayPal webhook signature here (optional but recommended)
-
         // Check if payment is completed
-        if ($data['event_type'] === 'PAYMENT.CAPTURE.COMPLETED') {
-            DB::beginTransaction();
-            try {
-                // Extract necessary information
-                $payerEmail = $data['resource']['payer']['email_address'];
-                $orderId = $data['resource']['supplementary_data']['related_ids']['order_id'];
-
-                // Retrieve form data using order ID or email or other unique identifier
-                $billingInfo = $data['resource']['billing_info'];  // or use session to retrieve it
-
-                // Perform the same actions as in the create method (save order, update stock, send emails)
-                $this->processOrder($billingInfo, $orderId, $payerEmail);
-
-                Log::info($billingInfo);
-
-                DB::commit();
-            } catch (\Throwable $th) {
-                DB::rollBack();
-                Log::error($th->getMessage());
-                return response()->json(['error' => 'Internal Server Error'], 500);
-            }
+        if ($data['event_type'] === 'CHECKOUT.ORDER.APPROVED') {
+            $orderId = $data['resource']['purchase_units'][0]['reference_id'];
+            Log::info('Order id: ' . $orderId);
         }
-
-        return response()->json(['status' => 'success'], 200);
     }
 
-    private function processOrder($billingInfo, $orderId, $payerEmail)
-    {
-        // Implement the logic from the create() method in your original code
-    }
 }
