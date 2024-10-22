e245480b5d87f75a638029ce2974ce15287c92df
Art Kujtimi <edits@ajroni.com>
2024-10-22 17:12:05 +0200

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -53,64 +53,20 @@ class LoginController extends Controller
 
         $user_email = $request->email;
         $user_pass = $request->password;
-        $user_wp_attempt = false;
         $check_user = User::where('email', $user_email)->where('password', '!=', null)->first();
-        // if ($check_user)
-        //     $user_wp_attempt = true;
-        // else
-        $user_wp_attempt = Auth::guard('wordpress')->attempt([
-            'user_email' => $user_email,
-            'user_pass' => $user_pass
-        ]);
-        if ($check_user || $user_wp_attempt) {
-            $exist_user = User::where('email', $user_email)->first();
-            if ($check_user) {
-                if (Auth::attempt(['email' => $user_email, 'password' =>  $user_pass])) {
-                    if (!$check_user->profile->id) {
-                        $check_user->profile()->create([
-                            'avatar' => null,
-                            'fullname' => Auth::guard('wordpress')->user() ? Auth::guard('wordpress')->user()->display_name : null
-                        ]);
-                    }
-                    Auth::login($check_user);
-                } else {
-                    return redirect()->back()->with('error', 'These credentials do not match our records.');
-                }
-            } else if ($exist_user != null && $exist_user->count()) {
-                $exist_user->password = Hash::make($user_pass);
-                $exist_user->save();
 
-                if ($check_user && !$check_user->profile->id) {
-                    $exist_user->profile()->create([
-                        'avatar' => null,
-                        'fullname' => ' '
-                    ]);
-                }
-                Auth::login($exist_user);
+        $exist_user = User::where('email', $user_email)->first();
+        if ($check_user) {
+            if (Auth::attempt(['email' => $user_email, 'password' =>  $user_pass])) {
+                Auth::login($check_user);
             } else {
-                $user_wp = Auth::guard('wordpress')->user();
-                $new_user = new User();
-                // $response = Http::get('https://wordpress-749479-2599216.cloudwaysapps.com/wp-json/wp/v2/users/' . $user_wp->ID);
-                $response = Http::get('http://old.myluxehub.com/wp-json/wp/v2/users/' . $user_wp->ID);
-                $new_user->email = $user_wp->user_email;
-                $new_user->password = Hash::make($user_pass);
-                $new_user->wp_id = $user_wp->ID;
-                $new_user->isAdmin = key_exists('slug', $response->json());
-                $new_user->role = key_exists('slug', $response->json()) ? 'admin' : 'agent';
-                $new_user->save();
-
-                $user = User::find($new_user->id);
-                $user->profile()->create([
-                    'avatar' => null,
-                    'fullname' => $user_wp->display_name
-                ]);
-                Auth::login($new_user);
+                return redirect()->back()->with('error', 'These credentials do not match our records.');
             }
-            if ($request->wantsJson()) {
-                return response()->json(['token' => $exist_user->createToken('API Token')->plainTextToken, 'user' => $exist_user]);
-            }
-            return redirect(Redirect::intended('/home')->getTargetUrl());
         }
+        if ($request->wantsJson()) {
+            return response()->json(['token' => $exist_user->createToken('API Token')->plainTextToken, 'user' => $exist_user]);
+        }
+        return redirect(Redirect::intended('/home')->getTargetUrl());
 
         return redirect()->back()->with('error', 'These credentials do not match our records.');
     }
