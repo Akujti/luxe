c4a0f95f2bce864a8ad0b205cb99e8575d3d3b8f
Art Kujtimi <edits@ajroni.com>
2024-11-21 13:23:43 +0100

    Update TestController.php

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Jobs\MarkCouponExpired;
+use App\Mail\DailyComingSoonListingsMail;
 use App\Mail\LowStockMail;
 use App\Models\Collection;
+use App\Models\Listing;
 use App\Models\LuxeStore\LuxeStoreCouponCode;
 use App\Models\LuxeStore\LuxeStoreProduct;
 use App\Notifications\CollectionShareNotification;
@@ -12,6 +14,7 @@ use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Notification;
 use Illuminate\Support\Facades\Storage;
@@ -19,7 +22,17 @@ use PDF;
 
 class TestController extends Controller
 {
-    public function index() {}
+    public function index()
+    {
+        Config::set('mail.mailers.smtp.host', 'smtp.office365.com');
+        Config::set('mail.mailers.smtp.port', 587);
+        Config::set('mail.mailers.smtp.username', 'noreply@luxeknows.com');
+        Config::set('mail.mailers.smtp.password', 'zpqhszrxplbjhhbc');
+        Config::set('mail.mailers.smtp.encryption', 'tls');
+        $listings = Listing::whereDate('created_at', Carbon::yesterday())->get();
+        Mail::to('art@ajroni.com')->send(new DailyComingSoonListingsMail($listings));
+        return 123;
+    }
 
     public function update_vimeo() {}
 
